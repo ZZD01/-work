09b90b3d1a62fe20a8e891c876b00d95b3f4e1fa
ZZD01 <71304192+ZZD01@users.noreply.github.com>
2024-05-31 11:15:16 +0800

    Add files via upload
    测试

---
+++ vue.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  chainWebpack: (config) => {
+    config.plugin("html").tap((args) => {
+      args[0].title = "Is点餐系统";
+      return args;
+    });
+  },
+  configureWebpack: {
+    resolve: {
+      extensions: [".js", ".json", ".vue", ".scss", ".css"],
+    },
+    externals: {
+      vue: "Vue",
+      "vue-router": "VueRouter",
+      // vuetify: "Vuetify",
+      axios: "axios",
+    },
+  },
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'https://api.chatanywhere.tech',
+        changeOrigin: true,
+        pathRewrite: { '^/api': '' }
+      }
+    }
+  }
+};
